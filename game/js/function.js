b97f11d9accaba4bafc9f1bab71ed6bc7c43c115
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2019-01-29 16:59:36 +0100

    started adding function courierCall

--- game/js/function.js
+++ game/js/function.js
@@ -1,28 +1,25 @@
 function makeBoard(target, size) {
     for (let y = 0; y < size; y += 1) {
-      let rowNode = createNode("row");
-  
-      for (let x = 0; x < size; x += 1) {
-        let cellNode = createNode("cell");
-  
-        rowNode.appendChild(cellNode);
-      }
-  
-      target.appendChild(rowNode);
+        let rowNode = createNode("row");
+        for (let x = 0; x < size; x += 1) {
+            let cellNode = createNode("cell");
+            rowNode.appendChild(cellNode);
+        }
+        target.appendChild(rowNode);
     }
-  }
-  
-  function createNode(className) {
+}
+
+function createNode(className) {
     const node = document.createElement("div");
     node.classList.add(className);
-  
     return node;
-  }
-  
-  function getIndexWithinParent(element) {
+}
+
+function getIndexWithinParent(element) {
     return Array.from(element.parentNode.children).indexOf(element);
-  }
-  
-  function getNextRow(element) {
+}
+
+function getNextRow(element) {
     return element.parentElement.nextElementSibling
-  }
+}
+
--- game/js/game.js
+++ game/js/game.js
@@ -1,3 +1,7 @@
 "use strict";
 const board = document.querySelector("#board");
 makeBoard(board, 10);
+
+function courierCall() {
+    const houses = document.querySelectorAll('house');
+}
