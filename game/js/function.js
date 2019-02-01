17abcfc50f4290d400266cd2217ec5c25d434ef1
daniel-mikula <daniel.mikula11@gmail.com>
2019-02-01 16:45:55 +0100

    timer not ready

--- game/js/function.js
+++ game/js/function.js
@@ -57,3 +57,16 @@ function courierCall() {
        houses[randomPackage].appendChild(pack);
   }
 }
+
+
+let timeLeft=60;
+let countDown = setInterval(function(){
+  timeLeft-=1;
+  document.getElementById('countdown').textContent=timeLeft + ' seconds left';
+  console.log(timeLeft);
+  if (timeLeft<=0){
+    document.getElementById('countdown').textContent=' Time is up!';
+    clearInterval(countDown)
+  }
+},100)
+
